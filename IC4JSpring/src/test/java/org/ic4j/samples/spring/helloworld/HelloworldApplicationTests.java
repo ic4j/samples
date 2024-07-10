a78b7e5cdc673980a92186e3955b6b912dc0c4db
Roman Dobrik <roman.dobrik@exilor.com>
2024-07-10 12:35:26 +0200

    Renamed

--- IC4JSpring/src/test/java/org/ic4j/samples/spring/helloworld/HelloworldApplicationTests.java
+++ IC4JSpring/src/test/java/org/ic4j/samples/spring/helloworld/HelloworldApplicationTests.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class HelloworldApplicationTests {
+class HelloWorldApplicationTests {
 	@Autowired
 	private MockMvc mvc;
 
