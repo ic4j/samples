beb39999323770e0603498f96b8d381c31050c65
Roman Dobrik <roman.dobrik@exilor.com>
2024-07-10 12:46:21 +0200

    new file

---
+++ IC4JSpring/src/test/java/org/ic4j/samples/spring/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,31 @@
+package org.ic4j.samples.spring.helloworld;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloWorldApplicationTests {
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void postGreet() {
+			try {
+				mvc.perform(MockMvcRequestBuilders.post("/greet").contentType(MediaType.TEXT_PLAIN).content("Motoko"))
+						.andExpect(status().isOk()).andExpect(content().string("Hello, Motoko!"));
+				Assertions.assertThat(true).isTrue();
+			} catch (Exception ex) {
+				Assertions.fail("Exception: " + ex.getMessage());
+			}
+	}
+
+}
